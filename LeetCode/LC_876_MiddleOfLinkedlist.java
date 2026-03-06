322dca1b614911e38937462a1125b8d66b7e3793
AarthiRagavan1 <aarthi@mukavai.com>
2026-03-06 15:03:11 +0530

    Linkedlist_problem

---
+++ LeetCode/LC_876_MiddleOfLinkedlist.java
@@ -0,0 +1,30 @@
+class LC_876_MiddleOfLinkedlist {
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null&& fast.next.next !=null){
+             slow=slow.next;
+             fast=fast.next.next;
+        }
+        if(fast.next==null){
+            return slow;
+
+        }
+        else{
+            return slow.next;
+        }
+    }
+}
+    
+
